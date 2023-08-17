be469bf4771e645d6b9d5534893a280bed994b92
FrostedCA <contact.tristan.landry@gmail.com>
2023-08-17 19:39:55 -0400

    EvoveePackets v1.8 Removed module-info.java (JitPack doesn't seems to like it)

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module EvoveePackets {
-
-    requires com.fasterxml.jackson.databind;
-    requires com.fasterxml.jackson.core;
-    requires com.fasterxml.jackson.annotation;
-
-    exports ca.tristan.evoveepackets;
-}
