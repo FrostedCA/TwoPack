d7d0192831c4b3d382eae4245881014a78869bcd
FrostedCA <contact.tristan.landry@gmail.com>
2023-08-17 19:03:52 -0400

    EvoveePackets v1.74 Added 'requires' tags

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,3 +1,7 @@
 module EvoveePackets {
+    requires com.fasterxml.jackson.databind;
+    requires com.fasterxml.jackson.core;
+    requires com.fasterxml.jackson.annotation;
+
     exports ca.tristan.evoveepackets;
 }
